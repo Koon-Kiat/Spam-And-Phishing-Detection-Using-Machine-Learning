0a81de42f50c71f418ffa45b347e7171049828e4
Koon Kiat <bookoonkiat@gmail.com>
2024-09-19 17:55:01 +0800

    Refactor code for improved readability and maintainability

--- ShapAnalysis.py
+++ ShapAnalysis.py
@@ -11,14 +11,14 @@ from sklearn.pipeline import Pipeline
 from tqdm import tqdm
 from sklearn.utils import shuffle
 import time
-import matplotlib.pyplot as plt  # Plotting library
+import matplotlib.pyplot as plt
 
 
-
+# Configure logging
 logging.basicConfig(format='%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s ', level=logging.INFO)
 
 
-
+# Define paths to models, data, and feature names
 base_dir = os.path.dirname(os.path.abspath(__file__))
 model_paths = [os.path.join(base_dir, 'Models & Parameters', f'ensemble_model_fold_{i+1}.pkl') for i in range(3)]
 X_test_paths = [os.path.join(base_dir, 'Feature Extraction', f'fold_{i+1}_test_data.npz') for i in range(3)]
@@ -54,11 +54,13 @@ column_names = categorical_columns + numerical_columns
 for fold in range(3):
     logging.info(f'Processing fold {fold+1}')
     
+
     # Load the model for this fold
     start_time = time.perf_counter()
     model = joblib.load(model_paths[fold])
     logging.info(f'Loaded model from {model_paths[fold]}')
     logging.info(f'Time taken to load model: {time.perf_counter() - start_time:.2f} seconds\n')
+
 
     # Load the test data
     start_time = time.perf_counter()
@@ -66,17 +68,20 @@ for fold in range(3):
     logging.info(f'Loaded test data from {X_test_paths[fold]}')
     logging.info(f'Time taken to load test data: {time.perf_counter() - start_time:.2f} seconds\n')
 
+
     # Load the training data to extract feature names
     start_time = time.perf_counter()
     X_train_combined = np.load(features_path[fold])['data']
     logging.info(f'Loaded train data from {features_path[fold]}')
     logging.info(f'Time taken to load train data: {time.perf_counter() - start_time:.2f} seconds\n')
 
+
     # Load the preprocessor
     start_time = time.perf_counter()
     preprocessor = joblib.load(preprocessor_path[fold])
     logging.info(f'Loaded preprocessor')
     logging.info(f'Time taken to load preprocessor: {time.perf_counter() - start_time:.2f} seconds\n')
+
 
     # Extract feature names
     start_time = time.perf_counter()
@@ -86,9 +91,11 @@ for fold in range(3):
     logging.info(f'Extracted feature names')
     logging.info(f'Time taken to extract feature names: {time.perf_counter() - start_time:.2f} seconds\n')
 
+
     # Define a function to predict using the model
     def predict_fn(X):
         return model.predict_proba(X)
+
 
     # Reduce the number of background samples using ksample
     start_time = time.perf_counter()
@@ -98,6 +105,7 @@ for fold in range(3):
     background_samples = shap.sample(X_train_sample, 100)  # Perform ksample with reduced sample size
     logging.info('Completed ksample sampling.')
     logging.info(f'Time taken for ksample sampling: {time.perf_counter() - start_time:.2f} seconds\n')
+
 
     # Initialize SHAP explainer based on model type
     start_time = time.perf_counter()
@@ -111,6 +119,7 @@ for fold in range(3):
     logging.info('Initialized SHAP Explainer.')
     logging.info(f'Time taken to initialize SHAP Explainer: {time.perf_counter() - start_time:.2f} seconds\n')
 
+
     # Calculate SHAP values
     start_time = time.perf_counter()
     logging.info('Calculating SHAP values...')
@@ -118,6 +127,7 @@ for fold in range(3):
     shap_values = explainer.shap_values(X_test_subset, check_additivity=False)
     logging.info('Completed SHAP value calculation.')
     logging.info(f'Time taken to calculate SHAP values: {time.perf_counter() - start_time:.2f} seconds\n')
+
 
     # Generate SHAP summary plot
     logging.info('Generating SHAP summary plot...')
@@ -127,6 +137,7 @@ for fold in range(3):
     plt.savefig(plot_path)
     logging.info(f'SHAP summary plot saved to {plot_path}.')
     plt.close()  # Close the plot to free up memory
+
 
     # If it's the first fold, you might want to stop early for testing
     if fold == 0:
