0c1a15fad3e47a3d7e72a16bc5d8a262f90ed868
Koon Kiat <bookoonkiat@gmail.com>
2025-02-13 04:40:42 +0800

    Implement data preprocessing pipelines for CEAS and SpamAssassin datasets

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import logging
 import pandas as pd
 from datasets import load_dataset
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
 
 from src.spamandphishingdetection import (
     load_config,
     check_config,
     get_file_paths,
+    log_label_percentages,
+    DatasetProcessor,
+    LabelMapper,
+    build_ceas_pipeline,
+    build_spamassassin_pipeline,
     initialize_environment
 )
 
@@ -22,16 +29,33 @@ def main():
     config = check_config(config, required_keys)
     file_path = get_file_paths(config)
 
-    logging.info("Application is ready to proceed.")
+    logging.info("Model Training begining...\n")
 
     # Load the ceas dataset
     df_ceas = pd.read_csv(
         file_path['ceas_08_dataset'], sep=',', encoding='utf-8')
-    
+
     # Load the spamassassin dataset
     dataset = load_dataset('talby/spamassassin',
                            split='train', trust_remote_code=True)
     df_spamassassin = dataset.to_pandas()
+
+    # Build pipelines for each dataset using the custom transformers.
+    spam_pipeline = build_spamassassin_pipeline(
+        file_path['preprocessed_spam_assassin_file'])
+    ceas_pipeline = build_ceas_pipeline(file_path['preprocessed_ceas_file'])
+
+    # * Data Preprocessing *#
+
+    # Process the datasets through their respective pipelines.
+    df_processed_spamassassin = spam_pipeline.fit_transform(df_spamassassin)
+    df_processed_ceas = ceas_pipeline.fit_transform(df_ceas)
+
+    # Optionally, combine dataframes after consistent processing
+    combined_df = pd.concat([df_processed_spamassassin, df_processed_ceas])
+
+    # Here you can call additional functions (for missing-value checks, etc.)
+    # For example: check_missing_values(combined_df, 'Combined CEAS_08 and SpamAssassin')
 
 if __name__ == "__main__":
     main()
