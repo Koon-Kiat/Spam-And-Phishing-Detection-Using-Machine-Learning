47d9147f14697c60a7180f255b132df47506d500
Koon Kiat <bookoonkiat@gmail.com>
2025-02-24 21:43:47 +0800

    Remove unused modules and functions from evaluationonthirddataset and spamandphishingdetection

--- src/evaluationonthirddataset/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from .file_operations import load_config, get_file_paths
-from .feature_engineering import EmailHeaderExtractor, load_or_extract_headers
-from .pipeline import run_pipeline_or_load
-
-__version__ = "0.1.0"
